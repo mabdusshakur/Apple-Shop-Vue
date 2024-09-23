c179011f8f110e30ad7559317469bc826a4a2fdd
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-23 10:12:16 +0600

    Footer added

---
+++ resources/js/Components/Footer.vue
@@ -0,0 +1,132 @@
+<script setup>
+
+</script>
+<template>
+    <footer class="footer_dark">
+        <div class="footer_top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 col-sm-12">
+                        <div class="widget">
+                            <div class="footer_logo">
+                                <Link href="#"><img src="/public/user/assets/images/logo_light.png" alt="logo" /></Link>
+                            </div>
+                            <p>If you are going to use of Lorem Ipsum need to be sure there isn't hidden of text</p>
+                        </div>
+                        <div class="widget">
+                            <ul class="social_icons social_white">
+                                <li>
+                                    <Link href="https://www.facebook.com"><i class="ion-social-facebook"></i></Link>
+                                </li>
+                                <li>
+                                    <Link href="https://www.facebook.com"><i class="ion-social-twitter"></i></Link>
+                                </li>
+                                <li>
+                                    <Link href="https://www.facebook.com"><i class="ion-social-googleplus"></i></Link>
+                                </li>
+                                <li>
+                                    <Link href="https://www.facebook.com"><i class="ion-social-youtube-outline"></i>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="https://www.facebook.com"><i class="ion-social-instagram-outline"></i>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-6">
+                        <div class="widget">
+                            <h6 class="widget_title">Useful Links</h6>
+                            <ul class="widget_links">
+                                <li>
+                                    <Link href="/policy?type=about">About Us</Link>
+                                </li>
+                                <li>
+                                    <Link href="/policy?type=how to buy">How to Buy</Link>
+                                </li>
+                                <li>
+                                    <Link href="/policy?type=contact">Contact</Link>
+                                </li>
+                                <li>
+                                    <Link href="/policy?type=complain">Complain</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-6">
+                        <div class="widget">
+                            <h6 class="widget_title">Legals</h6>
+                            <ul class="widget_links">
+                                <li><a href="/policy?type=refund">Refund Policy</a></li>
+                                <li><a href="/policy?type=terms">Terms & Condition</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-6 col-sm-6">
+                        <div class="widget">
+                            <h6 class="widget_title">My Account</h6>
+                            <ul class="widget_links">
+                                <li><a href="#">Profile</a></li>
+                                <li><a href="#">Wish List</a></li>
+                                <li><a href="#">Cart List</a></li>
+                                <li><a href="#">Order History</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-sm-6">
+                        <div class="widget">
+                            <h6 class="widget_title">Contact Info</h6>
+                            <ul class="contact_info contact_info_light">
+                                <li>
+                                    <i class="ti-location-pin"></i>
+                                    <p>123 Street, Old Trafford, New South London , UK</p>
+                                </li>
+                                <li>
+                                    <i class="ti-email"></i>
+                                    <a href="mailto:info@sitename.com">info@apple.com</a>
+                                </li>
+                                <li>
+                                    <i class="ti-mobile"></i>
+                                    <p>+ 457 789 789 65</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bottom_footer border-top-tran">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <p class="mb-md-0 text-md-start text-center">© 2020 All Rights Reserved by Apple Shop</p>
+                    </div>
+                    <div class="col-md-6">
+                        <ul class="footer_payment text-lg-end text-center">
+                            <li>
+                                <Link href="#"><img src="/public/user/assets/images/visa.png" alt="visa"></Link>
+                            </li>
+                            <li>
+                                <Link href="#"><img src="/public/user/assets/images/discover.png" alt="discover"></Link>
+                            </li>
+                            <li>
+                                <Link href="#"><img src="/public/user/assets/images/master_card.png" alt="master_card">
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="#"><img src="/public/user/assets/images/paypal.png" alt="paypal"></Link>
+                            </li>
+                            <li>
+                                <Link href="#"><img src="/public/user/assets/images/amarican_express.png"
+                                    alt="amarican_express"></Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<style scoped></style>
