8c94488124093ffe4d0b87c01c1d6798e6a5cbc5
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-23 17:15:11 +0600

    login page added

---
+++ resources/js/Pages/Auth/LoginPage.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onMounted } from 'vue';
+
+import MenuBar from '../../Components/MenuBar.vue';
+import Footer from '../../Components/Footer.vue';
+import Login from '../../Components/Login.vue';
+
+import { preloader_hide, preloader_show } from '../../config';
+
+onMounted(() => {
+    setTimeout(() => {
+        preloader_hide();
+    }, 500);
+});
+
+</script>
+
+<template>
+    <MenuBar />
+    <Login />
+    <Footer />
+</template>
+
+<style scoped></style>
