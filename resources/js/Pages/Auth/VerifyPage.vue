4bc90f5fffd091f0e11a235d5dabaab9ab0ef1fd
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-23 17:15:16 +0600

    verify page added

---
+++ resources/js/Pages/Auth/VerifyPage.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onMounted } from 'vue';
+
+import MenuBar from '../../Components/MenuBar.vue';
+import Footer from '../../Components/Footer.vue';
+import Verify from '../../Components/Verify.vue';
+
+import { preloader_hide, preloader_show } from '../../config';
+
+onMounted(() => {
+    setTimeout(() => {
+        preloader_hide();
+    }, 500);
+});
+
+</script>
+
+<template>
+    <MenuBar />
+    <Verify />
+    <Footer />
+</template>
+
+<style scoped></style>
