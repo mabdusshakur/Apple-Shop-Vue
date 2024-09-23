1709378e3d1f92a6f8eaacd72ea9b4090bed3168
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-23 23:27:16 +0600

    profile added

---
+++ resources/js/Components/Profile.vue
@@ -0,0 +1,37 @@
+<script setup>
+import ProfileDetails from './Profile/ProfileDetails.vue';
+import Orders from './Profile/Orders.vue';
+</script>
+
+<template>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-12">
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="profile-tab" data-bs-toggle="tab"
+                            data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane"
+                            aria-selected="false">Profile</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link active" id="order-tab" data-bs-toggle="tab"
+                            data-bs-target="#order-tab-pane" type="button" role="tab" aria-controls="order-tab-pane"
+                            aria-selected="false">Orders</button>
+                    </li>
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane" id="profile-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
+                        <ProfileDetails />
+                    </div>
+                    <div class="tab-pane show active" id="order-tab-pane" role="tabpanel" aria-labelledby="order-tab"
+                        tabindex="0">
+                        <Orders />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<style scoped></style>
