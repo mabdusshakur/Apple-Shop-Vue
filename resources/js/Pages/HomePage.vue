77f08dbe08b944f71a7efe70e4db1f4f7ab43785
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-23 17:24:09 +0600

    home page added

---
+++ resources/js/Pages/HomePage.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { onMounted } from 'vue';
+
+import MenuBar from '../Components/MenuBar.vue';
+import Footer from '../Components/Footer.vue';
+import TopBrands from '../Components/TopBrands.vue';
+import TopCategories from '../Components/TopCategories.vue';
+import ExclusiveProducts from '../Components/ExclusiveProducts.vue';
+import HeroSlider from '../Components/HeroSlider.vue';
+import {preloader_hide, preloader_show} from '../config';
+
+onMounted(() => {
+    setTimeout(() => {
+       preloader_hide();
+    }, 500);
+});
+
+</script>
+
+<template>
+    <MenuBar />
+    <HeroSlider />
+    <TopCategories />
+    <ExclusiveProducts />
+    <TopBrands />
+    <Footer />
+</template>
+
+<style scoped></style>
