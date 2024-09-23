8e012f2fa2f2f765d18a8f80683372f767cccaef
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-23 22:44:07 +0600

    Details page added

---
+++ resources/js/Pages/DetailsPage.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { onMounted } from 'vue';
+
+import MenuBar from '../Components/MenuBar.vue';
+import Footer from '../Components/Footer.vue';
+import TopBrands from '../Components/TopBrands.vue';
+import ProductDetails from '../Components/ProductDetails.vue';
+
+import { preloader_hide, preloader_show } from '../config';
+
+onMounted(() => {
+    setTimeout(() => {
+        preloader_hide();
+    }, 500);
+});
+
+</script>
+
+<template>
+    <MenuBar />
+    <ProductDetails />
+    <TopBrands />
+    <Footer />
+</template>
+
+<style scoped></style>
