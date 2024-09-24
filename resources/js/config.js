260dbc00a3d1eee42fd721bbb006e15ffc1b74d6
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-24 09:29:05 +0600

    getCookie function added

--- resources/js/config.js
+++ resources/js/config.js
@@ -5,3 +5,9 @@ export function preloader_hide() {
 export function preloader_show() {
     $(".preloader").delay(90).fadeOut(100).removeClass('loaded');
 }
+
+export function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
